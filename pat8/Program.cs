03dff4fc9a709207e852d3faa35a5062966cdcfb
Artem Mirvoda <mirvodartem@gmail.com>
2022-01-30 12:51:11 +0500

    Закончил с 8 лабой, пока что это самая дебильная лаба из всех

--- pat8/Program.cs
+++ pat8/Program.cs
@@ -15,26 +15,28 @@
     internal DateTime creationDate;
     public string GetCreationDate() => creationDate.ToShortDateString();
 
-    public override string ToString() => $"\t{GetName()}\n\r{GetDescription()}\n\r\n\r{GetCreationDate()} {GetCreatorPosition()} {GetCreatorFullName()}";
+    public override string ToString() => $"\t{GetName()}\t\t\n\r{GetDescription()}\t\n\r\n\r{GetCreationDate()} {GetCreatorPosition()} {GetCreatorFullName()}";
 }
 public class RussianDocument : Document
 {
     public new string GetCreationDate() => creationDate.ToString("dd.MM.yyyy");
-    public override string ToString() => $"Русский документ: {base.ToString()}";
+    public override string ToString() => $"Русский документ:\n\r{base.ToString()}";
 }
 public class EnglishDocument : Document
 {
     public new string GetCreationDate() => creationDate.ToString("MM-dd-yyyy");
-    public override string ToString() => $"Английский документ: {base.ToString()}";
+    public override string ToString() => $"Английский документ:\n\r{base.ToString()}";
 }
 public class EnglishDocumentAdapter : RussianDocument
 {
+    public EnglishDocumentAdapter(EnglishDocument document) => this.document = document;
     private EnglishDocument document;
     public new string GetName() => document.GetName();
     public new string GetDescription() => document.GetDescription();
     public new string GetCreatorPosition() => document.GetCreatorPosition();
     public new string GetCreatorFullName() => document.GetCreatorFullName();
     public new string GetCreationDate() => document.GetCreationDate();
+    public override string ToString() => $"Русский документ:\n\r\t{GetName()}\t\t\n\r{GetDescription()}\t\n\r\n\r{GetCreationDate()} {GetCreatorPosition()} {GetCreatorFullName()}";
 }
 public static class Programm
 {
@@ -44,11 +46,6 @@ public static class Programm
     {
         {1, "Начальник" },
         {2, "Преподаватель" },
-    };
-    public static Dictionary<int, (Type type, string name)> DocumentTypes = new Dictionary<int, (Type type, string name)>()
-    {
-        {1, (typeof(RussianDocument), "Русский документ") },
-        {2, (typeof(EnglishDocument), "Английский документ") },
     };
 
     public static void ConsoleReader()
@@ -61,31 +58,48 @@ public static class Programm
                 switch (Console.ReadLine())
                 {
                     case "1":
-                        Console.WriteLine($"Введите идентификатор документа: " +
-                            $"\n\r\t{DocumentTypes.Keys.Select(k => $"{k} - ").Zip(DocumentTypes.Values.Select(v => v.name))}\n\r");
-                        var documentType = DocumentTypes[Convert.ToInt32(Console.ReadLine())].type;
-                        
-                        Console.WriteLine($"Введите идентификатор должности составителя: " +
-                            $"\n\r\t{Positions.Keys.Select(k => $"{k} - ").Zip(Positions.Values)}\n\r");
-                        var creatorPosition = Positions[Convert.ToInt32(Console.ReadLine())];
-                        
-                        Console.WriteLine("Введите ФИО составителя:");
-                        var creatorName = Console.ReadLine();
-
-                        Console.WriteLine("Введите дату составления:");
-                        var creationDate = Convert.ToDateTime(Console.ReadLine());
-
                         Console.WriteLine("Введите текст документа:");
                         var description = Console.ReadLine();
 
                         Console.WriteLine("Введите название документа:");
                         var name = Console.ReadLine();
 
-                        Documents.Add(name, documentType.)
+                        Console.WriteLine("Введите ФИО составителя:");
+                        var creatorFullName = Console.ReadLine();
+
+                        Console.WriteLine("Введите дату составления:");
+                        var creationDate = Convert.ToDateTime(Console.ReadLine());
+
+                        Console.WriteLine($"Введите идентификатор должности составителя: " +
+                            $"\n\r\t{String.Join("\n\r\t", Positions.Select(p => $"{p.Key} - {p.Value}"))}\n\r");
+                        var creatorPosition = Positions[Convert.ToInt32(Console.ReadLine())];
+
+                        Console.WriteLine($"Введите идентификатор документа: " +
+                            $"\n\r\t1 - Русский документ" +
+                            $"\n\r\t2 - Английский документ");
+                        var documentType = Convert.ToInt32(Console.ReadLine());
+                        if (documentType == 1)
+                            Documents.Add(name, new RussianDocument()
+                            {
+                                name = name,
+                                creationDate = creationDate,
+                                description = description,
+                                creatorPosition = creatorPosition,
+                                creatorFullName = creatorFullName,
+                            });
+                        if (documentType == 2)
+                            Documents.Add(name, new EnglishDocument()
+                            {
+                                name = name,
+                                creationDate = creationDate,
+                                description = description,
+                                creatorPosition = creatorPosition,
+                                creatorFullName = creatorFullName,
+                            });
                         break;
                     case "2":
-                        Console.WriteLine("Введите дату начала учебы группы (дд.мм.гггг):");
-                        CurrentStudentGroup.Admission = Convert.ToDateTime(Console.ReadLine());
+                        Console.WriteLine("Введите название документа которы необходимо удалить:");
+                        Documents.Remove(Console.ReadLine());
                         break;
                     case "exit":
                         return;
@@ -100,10 +114,23 @@ public static class Programm
                         }
                 }
                 Console.ForegroundColor = ConsoleColor.Green;
-                var currentStudentGroupInfo = currentStudentGroup is null ? "Группа не создана" : currentStudentGroup.ToString();
-                Console.WriteLine($"Информация о группе студентов:" +
-                    $"\n\r{currentStudentGroupInfo}");
-                Console.ResetColor();
+
+                Console.WriteLine($"Имеющиеся документы: " +
+                            $"\n\r{String.Join("\n\r", Documents.Select(n => n.Value.ToString()))}\n\r");
+                Console.WriteLine($"Имеющиеся документы после адаптации:");
+                Documents.ToList().ForEach(n => {
+                    if (n.Value is EnglishDocument)
+                        Console.WriteLine(new EnglishDocumentAdapter((EnglishDocument)n.Value));
+                    else Console.WriteLine(n.Value);
+                });
+                    //Console.WriteLine($"Имеющиеся документы после адаптации: " +
+                    //            $"\n\r{String.Join("\n\r", Documents.Select(n => { 
+                    //                if (n.Value is EnglishDocument)
+                    //                    return new EnglishDocumentAdapter((EnglishDocument)n.Value).ToString();
+                    //                return n.Value.ToString();
+                    //            }))}\n\r");
+
+                    Console.ResetColor();
             }
             catch (Exception e)
             {
